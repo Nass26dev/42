999e6d89dc5864884a8b0db69b9e89336d35d721
Nass <nassyous26500@gmail.com>
2025-02-12 23:40:35 +0100

    remove obj deps

--- fdf/make/deps/render.d
+++
@@ -1,6 +0,0 @@
-make/objs/render.o: src//render.c src//../include/fdf.h \
- src//../include/../lib/libft/libft.h \
- src//../include/../lib/minilibx/mlx.h
-src//../include/fdf.h:
-src//../include/../lib/libft/libft.h:
-src//../include/../lib/minilibx/mlx.h:
