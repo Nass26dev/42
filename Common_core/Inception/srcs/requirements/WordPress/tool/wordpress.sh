a360a56dc3c7653a1e35955b1b810b17c06f2015
Nassim Yousfi <nyousfi@z4r8p6.42lyon.fr>
2025-12-19 08:06:46 +0100

    wip

---
+++ Common_core/Inception/srcs/requirements/WordPress/tool/wordpress.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+cd /var/www/html
+
+if [ ! -f wp-config.php ]; then
+    wp core download --allow-root
+
+    wp config create \
+        --dbname=${MYSQL_DATABASE} \
+        --dbuser=${MYSQL_USER} \
+        --dbpass=${MYSQL_PASSWORD} \
+        --dbhost=${MYSQL_HOST} \
+        --allow-root
+
+    wp core install \
+        --url=${DOMAIN_NAME} \
+        --title="${WP_TITLE}" \
+        --admin_user=${WP_ADMIN_USER} \
+        --admin_password=${WP_ADMIN_PASSWORD} \
+        --admin_email=${WP_ADMIN_EMAIL} \
+        --skip-email \
+        --allow-root
+
+    wp user create \
+        ${WP_USER} \
+        ${WP_USER_EMAIL} \
+        --user_pass=${WP_USER_PASSWORD} \
+        --role=author \
+        --allow-root
+fi
+
+exec php-fpm7.4 -F
